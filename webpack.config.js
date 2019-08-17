42d55d59296de387d62cd2adf1f1664e76ed3c0c
Murat Kemaldar <murat.kemaldar@outlook.com>
2019-08-17 15:48:21 +0200

    adjust package / webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   entry: ["./src/index.js"],
   output: {
-    path: `${__dirname}/docs`, // docs because of github pages
+    path: `${__dirname}/docs`,
     filename: "bundle.js"
   },
   devtool: "sourcemap",
